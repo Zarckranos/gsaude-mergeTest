5aec7122ccea35de1aedde97e6dff21d89d0c8d3
Jéssica Guiot <jessicaguiot@MacBook-Pro-de-Jessica.local>
2022-07-02 13:28:04 -0300

    init integration

--- src/components/MedicineItem/index.js
+++ src/components/MedicineItem/index.js
@@ -29,21 +29,23 @@ function chooseBadge (situation, availableQuantity, date) {
 }
 
 const MedicineItem = ({ data, openModal }) => {
-  const { name, availableQuantity, medicineLeaflet, situation, date } = data
+  const {amountAvailable, situation } = data
+  const {name} = data.medicine
+  const date = "20/05/22"
   const navigation = useNavigation()
   
   return (
     <Container>
       <Box>
         <Title>{name}</Title>
-        {chooseBadge(situation, availableQuantity, date)}
+        {chooseBadge(situation, amountAvailable, date)}
       </Box>
       <Footer>
         <Buttons activeOpacity={0.7} onPress={() => navigation.navigate("PDFView", {medicine:name})}>
          <Ionicons name="document" size={25} color={"#767373"}/>
          <Text>Ler a bula</Text>
         </Buttons>
-        <Buttons activeOpacity={0.7} onPress={() => openModal({name, availableQuantity})}>
+        <Buttons activeOpacity={0.7} onPress={() => openModal({name, amountAvailable})}>
          <Ionicons name="create" size={30} color={"#767373"}/>
          <Text>Editar</Text>
         </Buttons>
